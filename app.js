b74f33815543b858f1c61210cce8b651bca392af
Swicho <santifonlop@hotmail.com>
2023-09-24 19:51:14 -0300

    terminado

--- app.js
+++ app.js
@@ -1,68 +1,56 @@
-/* Bring Some art peaces with offset */
-let URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/7000"
-/* Specific art peace */
-let URLSpecific = "https://collectionapi.metmuseum.org/public/collection/v1/objects/7000"
 const departments = ["American Decorative Arts","Ancient Near Eastern Art","Arms and Armor","Arts of Africa, Oceania, and the Americas","Asian Art","The Cloisters","The Costume Institute","Drawings and Prints","Egyptian Art","European Paintings","European Sculpture and Decorative Arts","Greek and Roman Art","Islamic Art","The Robert Lehman Collection","The Libraries","Medieval Art","Musical Instruments","Photographs","Modern Art"]
-/*
-    Link
-    https://metmuseum.github.io/
-
-    Link DOCS - https://docs.google.com/document/d/1Xn4bbE-p7nBdwzr7fbaYE2159rLTbsWaiqf9IxFB11I/edit
-
-    
-    Metadata
-    https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=2018-10-22
-
-    Departments 
-    https://collectionapi.metmuseum.org/public/collection/v1/departments
-
-    Highlifhts 
-    https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=European%20Paintings
-
-    after time and nacionality
-
-    https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin=2000&q=african
-    
-    range between and nacionality
-    https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin=1700&dateEnd=1800&q=African
-
-    nationality is in english eg : Peruvian, uruguayan, mexican etc 
-*/
 
 const getFrontPageArt =async e =>{
-    const number = Math.floor(Math.random() * (departments.length - 0 + 1) + 0)
-    console.log(number)
-    console.log(departments[number])
-    const req = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=${departments[number]}`)
-    const resp = await req.json()
-    console.log(resp.objectIDs)
-    findData(resp.objectIDs)
+    const number = getRandomNumber(departments.length)
+    document.getElementById('showDataTitle').innerText = `Category: ${e?e:departments[number]}`
+    document.getElementById('data').innerHTML = `<img src="./imgs/loading.gif" style="width:50px;" />`
+    try{
+        const req = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${e?e:departments[number]}`)
+        const resp = await req.json()
+        findData(resp.objectIDs)
+    }catch(e){
+        console.log(e)
+    }
 }
 
 const findData = async (e)=>{
     const data = []
-    for (const i of e) {
-        // console.log('entre')
-        if(e[10]==i)break
-        const req = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${i}`)
-        const resp = await req.json()
-        data.push(resp)
-        console.log(await resp)
-        placeData(data)
+    
+    for (let i = 1; i<20; i++) {
+        const number = getRandomNumber(e.length)
+        try{
+            const req = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${number}`)
+            const resp = await req.json()
+            data.push(resp)
+        }catch(e){
+            console.log(e)
+        }
     }
+    placeData(data)
 
 }
 
 const placeData = (e) =>{
     let data = document.getElementById('data')
-    document.getElementById('data').innerHTML =""
-    console.log(e)
+    data.innerHTML = "" 
+    
     e.forEach(art => {
-        data.innerHTML += `<div>
-        <div><img src="${art.primaryImageSmall}"/></div>
+        if(!art.primaryImageSmall)return
+        data.innerHTML += `
+        <div class="artPeace">
+        <img src="${art.primaryImageSmall}"/>
         <div>${art.title}</div>
         </div>`
     });
+}
+
+ const parameters = document.getElementById('params')
+ parameters.addEventListener('change', (e) =>{
+    if(e.target.value === 'Department')return
+    getFrontPageArt(e.target.value)
+})
+const getRandomNumber = (e) =>{
+    return Math.floor(Math.random() * (e - 0 + 1) + 0)
 }
 
 getFrontPageArt()
